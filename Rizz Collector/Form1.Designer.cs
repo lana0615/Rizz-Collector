651474daac3efa1811de19165916ded4e4eb7a01
Alana Fortin <Alanfort577@ed.amdsb.ca>
2023-01-19 10:25:32 -0500

    Add project files.

---
+++ Rizz Collector/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Rizz_Collector
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.livesLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.gameLabel = new System.Windows.Forms.Label();
+            this.singleEnding = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(70, 26);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Score:";
+            // 
+            // livesLabel
+            // 
+            this.livesLabel.AutoSize = true;
+            this.livesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.livesLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livesLabel.ForeColor = System.Drawing.Color.White;
+            this.livesLabel.Location = new System.Drawing.Point(453, 9);
+            this.livesLabel.Name = "livesLabel";
+            this.livesLabel.Size = new System.Drawing.Size(60, 26);
+            this.livesLabel.TabIndex = 2;
+            this.livesLabel.Text = "Lives:";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.ForeColor = System.Drawing.Color.White;
+            this.instructionLabel.Location = new System.Drawing.Point(179, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(213, 26);
+            this.instructionLabel.TabIndex = 6;
+            this.instructionLabel.Text = "Press Esc to exit Game";
+            // 
+            // gameLabel
+            // 
+            this.gameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.gameLabel.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameLabel.ForeColor = System.Drawing.Color.White;
+            this.gameLabel.Location = new System.Drawing.Point(109, 84);
+            this.gameLabel.Name = "gameLabel";
+            this.gameLabel.Size = new System.Drawing.Size(377, 209);
+            this.gameLabel.TabIndex = 7;
+            this.gameLabel.Text = " Rizz Collector\r\n\r\nCollect:  ✅   Avoid: ❌\r\n\r\nPress Space bar to begin\r\n\r\n";
+            this.gameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // singleEnding
+            // 
+            this.singleEnding.BackColor = System.Drawing.Color.Transparent;
+            this.singleEnding.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.singleEnding.ForeColor = System.Drawing.Color.White;
+            this.singleEnding.Location = new System.Drawing.Point(109, 84);
+            this.singleEnding.Name = "singleEnding";
+            this.singleEnding.Size = new System.Drawing.Size(377, 209);
+            this.singleEnding.TabIndex = 8;
+            this.singleEnding.Text = "You were rejected three times therefore will be single forver\r\n\r\nPress Space bar " +
+    "to begin\r\n\r\n";
+            this.singleEnding.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.singleEnding);
+            this.Controls.Add(this.gameLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.livesLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "form1";
+            this.Text = "Rizz Collector";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label livesLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label gameLabel;
+        private System.Windows.Forms.Label singleEnding;
+    }
+}
+
---
+++ Rizz Collector/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Alana Fortin:January 24th 
+// a variation on the catch game we made
+// one player
+// catches "rizz" and gets dating tips
+// if player collides with "rejection" they loose a life
+// 3 lives lost and game is over
+
+namespace Rizz_Collector
+{
+    public partial class form1 : Form
+    {
+        Rectangle player = new Rectangle(540,200,40,10);
+        int playerSpeed = 10;
+
+        List<Rectangle> rizz = new List<Rectangle>();
+        List<int> rizzSpeed = new List<int>();
+        List<string> rizzColor = new List<string>();
+
+        List<Rectangle> rejections = new List<Rectangle>();
+        List<int> rejectionsSpeed = new List<int>();
+        List<string> rejectionColor = new List<string>();
+
+
+        int rizzSpeeds = 10;
+        int rejectionSpeed = 10;
+
+        int score = 0;
+
+        int groundHeight = 150;
+
+        bool leftDown = false;
+        bool rightDown = false;
+        bool escDown = false;
+
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush pinkBrush = new SolidBrush(Color.HotPink);
+        SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        string gameState = "waiting";
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameSetup()
+        {
+            gameState = "running";
+
+            instructionLabel.Visible = false; 
+
+            gameLoop.Enabled = true;
+            score = 0;
+
+            player.X = 280;
+            rizz.Clear();
+            rizzSpeed.Clear();
+            rejections.Clear();
+            rejectionsSpeed.Clear();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameSetup();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over" || gameState == "running")
+                    {
+                        this.Close();
+                    }
+                    break;
+            }
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            //move player
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //game state = waiting
+            if (gameState == "waiting")
+            {
+                scoreLabel.Visible = false;
+                livesLabel.Visible = false;
+         //       bottomBar.Visible = false;
+                gameLabel.Visible = true;
+                singleEnding.Visible = false;
+                instructionLabel.Visible = true;
+            }
+            else if (gameState == "running")
+            {
+                //update labels 
+                //bottomBar.Visible = true;
+                scoreLabel.Visible = true;
+                livesLabel.Visible = true;
+                gameLabel.Visible = false;
+                singleEnding.Visible = false;
+                instructionLabel.Visible = false;
+
+                gameLabel.Enabled = false;
+                singleEnding.Enabled = false;
+                instructionLabel.Enabled = false;
+
+                //draw a rectangle at the bottom to represent the ground
+                e.Graphics.FillRectangle(purpleBrush, 0, this.Height - groundHeight, this.Width, groundHeight);
+
+
+                //draw player
+                e.Graphics.FillRectangle(pinkBrush, player);
+
+                //draw  rizz
+                //for (int i = 0; i < rizz.Count(); i++)
+                //{
+                //    if (rizzColor[i] == "green")
+                //    {
+                //        e.Graphics.FillEllipse(greenBrush, rizz[i]);
+                //    }
+                //}
+
+                //// draw rejections
+                //for (int i = 0; i < rejections.Count(); i++)
+                //{
+                //    if (rejectionColor[i] == "red")
+                //    {
+                //        e.Graphics.FillEllipse(redBrush, rejections[i]);
+                //    }
+                //}
+            }
+            else if (gameState == "over")
+            {
+                scoreLabel.Visible = false;
+                livesLabel.Visible = false;
+          //      bottomBar.Visible = false;
+                singleEnding.Visible = true;
+                instructionLabel.Visible = true;
+            }
+        }
+    }
+}
